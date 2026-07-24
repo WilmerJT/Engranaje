0db75c4ecb23f22311076ad9da2c968d09963d1f
andresdanilo11 <andicandop@gmail.com>
2026-07-24 18:26:49 -0500

    feat(math): crear estructura del módulo gearMath

---
+++ services/gearMath.js
@@ -0,0 +1,33 @@
+function calculateDimensions(){}
+
+function involutePoint(){}
+
+function generateInvolute(){}
+
+function rotatePoint(){}
+
+function mirrorInvolute(){}
+
+function buildTooth(){}
+
+function buildGear(){}
+
+
+
+module.exports = {
+
+    calculateDimensions,
+
+    involutePoint,
+
+    generateInvolute,
+
+    rotatePoint,
+
+    mirrorInvolute,
+
+    buildTooth,
+
+    buildGear
+
+};
